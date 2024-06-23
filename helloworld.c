4bd1c5691b21f0d92bddea4260b04e51733c18be
maehw <maehw@posteo.de>
2024-06-23 17:55:12 +0200

    Use some other text for the 'hello world' example

--- helloworld.c
+++ helloworld.c
@@ -2,8 +2,16 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-  cputs("maehw");
-  sleep(10);
+  cputs("hello");
+  sleep(1);
+  cputs("world");
+  sleep(2);
+  cputs("git");
+  sleep(3);
+  cputs("hub");
+  sleep(2);
+  cputs("demo");
+  sleep(1);
   cls();
   
   return 0;
